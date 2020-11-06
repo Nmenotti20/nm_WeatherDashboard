216783169a5c212cb7fe556210dd00fbf5b57ac1
Nickolas Menotti <nmenotti@cox.net>
2020-11-05 17:55:54 -0800

    added vars to script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+var citiesArray;
+var makeClickable;
+var uvColors = ["green" ,"green", "green", "yellow", "yellow", "yellow", "orange", "orange", "red", "red", "red", "purple", "purple", "purple"];
+$("#search-btn").on("click", function(e) {
+    
+    var APIKey = "ae2dcc589707f61a832c268641e09317";
+    var city = $("#search").val();
