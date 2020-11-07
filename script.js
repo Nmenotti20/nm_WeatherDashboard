0478bc4b47cf262cc4ffea2919e0bc197c6f8dab
Nickolas Menotti <nmenotti@cox.net>
2020-11-06 19:29:29 -0800

    commented out script.js

--- script.js
+++ script.js
@@ -5,3 +5,34 @@ $("#search-btn").on("click", function(e) {
     
     var APIKey = "ae2dcc589707f61a832c268641e09317";
     var city = $("#search").val();
+
+    // Populate the Name of the Searched City in the list under the search field
+
+
+
+    // URL to query the api.openweathermap.org API
+
+
+
+    // AJAX call
+
+
+
+
+    // URL to query the apo.openweathermap.org API
+
+
+
+
+    // AJAX call
+
+
+
+
+    // AJAX call
+
+
+
+
+
+    // Local storage the cities that have been searched.
