39c4dd5cbf82c73e66de363f6271fa19f3109e03
Nickolas Menotti <nmenotti@cox.net>
2020-11-06 21:57:35 -0800

    added script.js and readme.md

--- README.md
+++ README.md
@@ -2,7 +2,61 @@
 Weather Dashboard using Server-Side APIs
 
 
+##Project Title: nm_WeatherDashboard - by Nick Menotti 	###Date: November 7, 2020
+ 
+###Table of Contents
 
-Project URL: https://nmenotti20.github.io/nm_WeatherDashboard/
-GitHub URL: https://github.com/Nmenotti20/nm_WeatherDashboard.git
+1.	Project Description
+2.	Installation
+3.	Usage
+4.	Credits
+5.	License
+ 
 
+####1.	Project Description
+Objective
+
+
+
+
+
+
+
+
+
+
+
+
+####2.	Installation
+Click on the link below to view the code for this project in GitHub: 
+Link to GitHub Repository: https://github.com/Nmenotti20/nm_WeatherDashboard.git
+
+
+Click on the link below to view the deployed URL for this project:
+Link to GitHub Pages Published url: https://nmenotti20.github.io/nm_WeatherDashboard/
+
+
+####3. Credits
+Special thanks to the following:
+
+MomentJS:
+https://momentjs.com/
+
+Bootstrap:
+https://getbootstrap.com/
+
+Tutorials:
+https://www.w3schools.com/
+
+Contributors:
+Enrique Gomez
+
+
+####4.	License
+MIT License
+Copyright (c) [2020] [Nickolas Menotti]
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- script.js
+++ script.js
@@ -6,33 +6,163 @@ $("#search-btn").on("click", function(e) {
     var APIKey = "ae2dcc589707f61a832c268641e09317";
     var city = $("#search").val();
 
-    // Populate the Name of the Searched City in the list under the search field
+    // Populate the Name of the Searched City in the list under the search field.
+    var array = [];
+    if (localStorage.getItem("cities")) {
+        JSON.parse(localStorage.getItem("cities")).map(item => array.push(item));
+        array.push(city);
+        localStorage.setItem("cities", JSON.stringify(array));
+    } else {
+        array.push(city);
+        console.log(array);
+        localStorage.setItem("cities", JSON.stringify(array));
+    }
+    console.log(localStorage.getItem("cities"));
 
+    // URL to query the api.openweathermap.org API.
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+    // AJAX call to retreive data from API. //
+    $.ajax({url: weatherURL, method: "GET"})
+     .then(function(response) {
+        console.log(response);
+        
+        var citiesDiv = $("#cities");
+        var newCityDiv = $("<div>");
+        newCityDiv.text(city);
+        newCityDiv.addClass("city py-3 pl-3 border-bottom");
+        citiesDiv.append(newCityDiv);
 
-    // URL to query the api.openweathermap.org API
+        $("#current-city").text(city)
+        var dateArray = $(".date");
+        var iconArray = document.querySelectorAll(".icon");
+        var tempArray = $(".temp");
+        var humidityArray = $(".humidity");
+        $("#wind-speed").text(Math.round((parseInt(response.wind.speed) * 2.2369) * 10) / 10 + " MPH");
 
+        dateArray[0].textContent = moment().format("l");
+        var iconURL = "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png";
+        iconArray[0].setAttribute("src", iconURL);
+        tempArray[0].textContent = (Math.round(((parseInt(response.main.temp) - 273.15) * (9/5) + 32) * 10) / 10 + " \u00B0F");
+        humidityArray[0].textContent = response.main.humidity + "%";
+        
+        // AJAX call to retreive data from API //
+        $.ajax({url: "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + APIKey, method: "GET"})
+         .then(function(response) {
+            console.log(response);
+            $("#uv-index").text(response[0].value);
+            $("#uv-index").css("background-color", uvColors[Math.floor(response[0].value)]);
+         })
 
+        var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+        
+        // AJAX call to retreive data from API //
+        $.ajax({url: forecastURL, method: "GET"})
+         .then(function(response) {
+            console.log(response);
+    
+                $("#wind-speed").text(Math.round((parseInt(response.list[0].wind.speed) * 2.2369) * 10) / 10 + " MPH");
+                
+                var k = 1;
+                var day = 1;
+                for (var i = 0; i < response.list.length; i++) {
+                    if (response.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+                        dateArray[k].textContent = moment().add(day, 'day').format("l");
+                        var iconURL = "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png";
+                        iconArray[k].setAttribute("src", iconURL);
+                        tempArray[k].textContent = (Math.round(((parseInt(response.list[i].main.temp) - 273.15) * (9/5) + 32) * 10) / 10 + " \u00B0F");
+                        humidityArray[k].textContent = response.list[i].main.humidity + "%";
+                        k+=1;
+                        day+=1;
+                    }
+                }
+                $("#container-right").removeClass("hide");
+            })
 
-    // AJAX call
+        makeClickable()
+    });
+});
 
+// When the button is clicked, populate the field with the current city's weather infomation //
+function makeClickable() {
+    citiesArray = document.querySelectorAll(".city");
+    for (var j = 0; j < citiesArray.length; j++) {
+        citiesArray[j].addEventListener("click", function(event) {
+            var APIKey = "ae2dcc589707f61a832c268641e09317";
+            var city = event.target.textContent;
+            console.log(city);
 
+    // URL to query the apo.openweathermap.org API. //
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+    // Vars for current-city //
+    $("#current-city").text(city);
+    var dateArray = $(".date");
+    var iconArray = document.querySelectorAll(".icon");
+    var tempArray = $(".temp");
+    var humidityArray = $(".humidity");
 
-    // URL to query the apo.openweathermap.org API
+    // AJAX call to retreive data from API //
+    $.ajax({url: weatherURL, method: "GET"})
+     .then(function(response) {
+        console.log(response);
 
+            $("#wind-speed").text(Math.round((parseInt(response.wind.speed) * 2.2369) * 10) / 10 + " MPH");            
+            
+            dateArray[0].textContent = moment().format("l");
+            var iconURL = "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png";
+            iconArray[0].setAttribute("src", iconURL);
+            tempArray[0].textContent = (Math.round(((parseInt(response.main.temp) - 273.15) * (9/5) + 32) * 10) / 10 + " \u00B0F");
+            humidityArray[0].textContent = response.main.humidity + "%";
 
+            // AJAX call to retreive data from API //
+            $.ajax({url: "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + APIKey, method: "GET"})
+             .then(function(response) {
+                console.log(response);
+                $("#uv-index").text(response[0].value);
+                $("#uv-index").css("background-color", uvColors[Math.floor(response[0].value)]);
+                })
+        })
+  
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
 
+    // AJAX call to retreive data from API //
+    $.ajax({ url: forecastURL, method: "GET"})
+     .then(function(response) {
+        console.log(response);
 
-    // AJAX call
+        $("#wind-speed").text(Math.round((parseInt(response.list[0].wind.speed) * 2.2369) * 10) / 10 + " MPH");
+        
+        var k = 1;
+        var day = 1;
+        for (var i = 0; i < response.list.length; i++) {
+            if (response.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+                dateArray[k].textContent = moment().add(day, 'day').format("l");
+                var iconURL = "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png";
+                iconArray[k].setAttribute("src", iconURL);
+                tempArray[k].textContent = (Math.round(((parseInt(response.list[i].main.temp) - 273.15) * (9/5) + 32) * 10) / 10 + " \u00B0F");
+                humidityArray[k].textContent = response.list[i].main.humidity + "%";
+                k+=1;
+                day+=1;
+            }
+        }
 
+        $("#container-right").removeClass("hide");
+    })
+})
+}
+}
 
-
-
-    // AJAX call
-
-
-
-
-
-    // Local storage the cities that have been searched.
+// Local storage the cities that have been searched. //
+if(localStorage.getItem("cities")) {
+    var savedCities = JSON.parse(localStorage.getItem("cities"));
+    console.log(savedCities);
+    for (var i = 0; i < savedCities.length; i++) {
+        var citiesDiv = $("#cities");
+        var newCityDiv = $("<div>");
+        newCityDiv.text(savedCities[i]);
+        newCityDiv.addClass("city py-3 pl-3 border-bottom");
+        citiesDiv.append(newCityDiv);
+     makeClickable();
+    }
+}
